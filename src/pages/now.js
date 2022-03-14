09ea8b7f2db7f8f3e4eec733747c61868cca5171
Luke Karrys <luke@lukekarrys.com>
2022-03-13 21:39:32 -0700

    now

--- src/pages/now.js
+++ src/pages/now.js
@@ -5,54 +5,22 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="Now" />
-    <h1>Here's what I'm up to right now (updated 2021-01-29):</h1>
+    <h1>Here's what I'm up to right now (updated 2022-03-13):</h1>
     <p>
-      <a href="https://www.instagram.com/stories/highlights/17870968583199212/">
-        Bike maintenance
-      </a>{" "}
-      – I'm attempting to completely take apart a fixed gear bicycle, get it
-      powder coated, and rebuild it with a mix of new and old parts. I'm
-      documenting it on Instagram stories and it's a slow process, but I'm
-      learning.
+      <a href="https://bracket.club">bracket.club</a> – It's bracket club season
+      again. I didn't do much to the site this year except get it up and running
+      but hopefully I can get some new ideas and tech into it for next year.
     </p>
     <p>
-      <a href="https://lukeswickedcoolkit.herokuapp.com">My Wicked Coolkit</a> –
-      I spent the last couples months creting the Wicked Coolkit with some
-      friends at &yet and Heroku/Salesforce. I'm really happy with how it turned
-      out! <a href="https://wickedcoolkit,com">You can make your own too!</a>
-    </p>
-    <p>
-      <a href="https://lukekarrys.com">lukekarrys.com</a> – My previous personal
-      site had a picture of me from my wedding day which is almost 3 kids ago. I
-      decided to update it with the color scheme I was using for my resume. I
-      tried to keep it simple and do fun/silly stuff while keeping it accessible
-      with <code>prefers-reduced-motion</code>.
-    </p>
-    <p>
-      <a href="/tags/bikes">bikes</a> &amp;{" "}
-      <a href="/tags/coffeeneuring">coffee</a> – After going through a running
-      injury, I had to find a different way to stay active and generate
-      seratonin to keep the brain baddies at bay. While I enjoyed the
-      competitive side of running and pushing to get faster for races, with
-      biking I've enjoyed slow commute/bikepacking style rides where I get to
-      haul gear, stop and make/buy coffee, and take pictures. I'm doing the
-      Coffeeneuring Challenge for 2020 which has been a great excuse to get out
-      on my bike.
+      <a href="https://github.com/npm/template-oss">@npmcli/template-oss</a> –
+      This is a templating project I worked on with my team to try and bring
+      some order to our 80-ish open source repos we manage. It's been a lot of
+      fun solving this problem and see the fruits of that labor.
     </p>
     <p>
       <a href="https://www.instagram.com/lukes.outside/">Instagram</a> – I made
       a public Instagram to share outdoor stuff that I like doing. So far it's
       mostly biking but I'm sure it will change with the (life) seasons.
-    </p>
-    <p>
-      <a href="https://livefromquarantine.club">livefromquarantine.club</a> –
-      Some musicians I like have been doing daily or weekly streams from home so
-      I built this site to make it easy to jump between songs in different
-      videos. (Over-)engineering the automated data pipeline for{" "}
-      <a href="https://livefromquarantine.club/bengibbard">multiple</a>{" "}
-      <a href="https://livefromquarantine.club/benfolds">artists</a> has been a
-      fun distraction right now. I also used it as an excuse to learn how state
-      machines and TypeScript work together and that was a wild ride.
     </p>
     <p>
       <a href="https://photos.lukelov.es">photos.lukelov.es</a> – A photo blog
